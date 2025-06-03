ed68ef2d69540f53396b80fe984fd0857984633c
Jeffin <jeffinj787@gmail.com>
2025-06-03 01:17:10 +0100

    chore: cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getInitialTruths } from "@/server/truth-service";
 
 export default async function Home() {
   const data = await getInitialTruths();
-  if (!data) throw new Error("No data available");
+  if (!data) throw new Error("API call to get initial truths failed");
   return (
     <div className="bg-[#050A0F] min-h-screen p-5 flex flex-col items-center justify-center">
         <TruthSocialFeed initialTruths={data} />
